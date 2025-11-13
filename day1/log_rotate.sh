42bdfb6438f8f16cca2424628faa5a608b125927
AmanPandey7 <Aman711pandey@gmail.com>
2025-11-13 19:23:41 +0530

    Adding logging to script

--- day1/log_rotate.sh
+++ day1/log_rotate.sh
@@ -9,3 +9,4 @@ TIMESTAMP=$(date +"%Y-%m-%d_%H-%M-%S")
 tar -czf $ARCHIVE_DIR/logs_$TIMESTAMP.tar.gz $LOG_DIR/*.log 2>/dev/null
 
 echo "Logs archived at $ARCHIVE_DIR/logs_$TIMESTAMP.tar.gz"
+echo 'Backup completed successfully'
